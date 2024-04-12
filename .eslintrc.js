ee1d036a6272ddc3ef3632f4dbc0c28f5911657c
dragonite <nguyentt.dev@gmail.com>
2024-04-12 22:23:32 +0200

    config eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['@react-native-community', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-var-requires': 0,
+  },
 };
